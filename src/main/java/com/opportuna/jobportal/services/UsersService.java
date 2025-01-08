343c8230428e6dcd103e074517bed031128527db
Hossam Hatem <7ossam7atem1@gmail.com>
2025-01-08 20:43:30 +0200

    chore: encrypt users passwords

--- src/main/java/com/opportuna/jobportal/services/UsersService.java
+++ src/main/java/com/opportuna/jobportal/services/UsersService.java
@@ -7,6 +7,7 @@ import com.opportuna.jobportal.repository.JobSeekerProfileRepository;
 import com.opportuna.jobportal.repository.RecruiterProfileRepository;
 import com.opportuna.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -17,17 +18,20 @@ public class UsersService {
     private UsersRepository usersRepository;
     private JobSeekerProfileRepository jobSeekerProfileRepository;
     private RecruiterProfileRepository recruiterProfileRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UsersService(UsersRepository usersRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository) {
+    public UsersService(UsersRepository usersRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository, PasswordEncoder passwordEncoder) {
         this.usersRepository = usersRepository;
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
         this.recruiterProfileRepository = recruiterProfileRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Users addNew(Users users) {
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        users.setPassword(passwordEncoder.encode(users.getPassword()));
         Users savedUser = usersRepository.save(users);
         int userTypeId = users.getUserTypeId().getUserTypeId();
         if (userTypeId == 1) {
